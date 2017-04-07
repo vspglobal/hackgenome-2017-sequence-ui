940962b1f9b923a84196cd5042a9194763c5df82
SethVa <Sethva@vsp.com>
2017-04-07 10:43:27 -0700

    Hack Genome 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  redirect_uri: 'http://40.71.86.58/redirect',
-  client_id:'HackGenome'
+  redirect_uri: 'http://40.71.86.58',
+  client_id:'HackGenome2'
 };
